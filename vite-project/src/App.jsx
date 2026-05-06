edf5caf57b802d495de9309542e0f5c472a5bee0
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-06 11:28:34 +0500

    vejof

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+const { useEffect } = require("react");
+
+const Card = () => {
+  const [data, setData] = userState([]);
+  useEffect(() => {
+    fetch ("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
+  })
+}
